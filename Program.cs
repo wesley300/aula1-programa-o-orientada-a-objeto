4a081e1dda33937447d7938b3d46866b776f4651
curso.ads <curso.ads@LAB06-44>
2022-08-15 22:04:58 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1_programação_orientada_a_objeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            // instanciar o objeto 
+            Veiculo golf = new Veiculo();
+           
+            Console.WriteLine("Digite a marca");
+            string marca=Console.ReadLine();
+            
+            Console.WriteLine("Digite a modelo");
+            string modelo = Console.ReadLine();
+            
+            Console.WriteLine("Digite a placa");
+            string placa = Console.ReadLine();
+            
+            Console.WriteLine("Digite o ano");
+            int ano = Convert.ToInt32(Console.ReadLine());
+            // objeto chamando método atribuir
+
+            golf.atribuir(marca, modelo, placa, ano);
+            Console.WriteLine(golf.retorna());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Veiculo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1_programação_orientada_a_objeto
+{
+    public class Veiculo
+    {
+        // declaração de atributos
+        string marca;
+        string modelo;
+        string placa;
+        int ano;
+        // método para atribuir valores 
+        
+        public void atribuir(string ma, string mo, string pl, int a)
+        {
+            this.ano = a;
+            this.marca= ma;
+            this.modelo= mo;
+            this.placa= pl;
+        }
+        public string retorna() {
+            return "Marca=" + this.marca +
+                "\nModelo=" + this.modelo +
+                "\nAno de fabricação=" + this.ano +
+                "\nPlaca=" + this.placa;
+        }
+    }   
+}
